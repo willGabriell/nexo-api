7b3872b407392efb0b23569e5e099beee60223e9
Wilinton <wilintongabrielss1@gmail.com>
2025-05-02 15:07:46 -0300

    init: commit inicial

---
+++ src/main/java/com/nexo/NexoApplication.java
@@ -0,0 +1,13 @@
+package com.nexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoApplication.class, args);
+	}
+
+}
