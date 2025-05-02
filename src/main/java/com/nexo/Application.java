8bfa32be73e2067f72ac18acbdf7081389a3b779
Wilinton <wilintongabrielss1@gmail.com>
2025-05-02 15:55:13 -0300

    config: configurações iniciais do projeto

--- src/main/java/com/nexo/Application.java
+++ src/main/java/com/nexo/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NexoApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(NexoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
